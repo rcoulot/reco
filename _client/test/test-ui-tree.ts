2cdb3a8afc6fd44c0623510a21d84ed62ca02bb6
rcoulot <rcoulot@gmail.com>
2025-10-02 02:01:56 +0200

    tree todo parent as id instead of HTMLElement

--- _client/test/test-ui-tree.ts
+++ _client/test/test-ui-tree.ts
@@ -3,12 +3,15 @@ namespace reco.core.ui.tree.test {
     import TestDB = reco.core.test.data.TestDB
     import testPDBJson = reco.core.test.data.testPDBJson
     import GeoTreeHandler = reco.core.test.data.GeoTreeHandler
+    import LayoutNcSc = reco.ui.layout.LayoutNcSc;
     // ============================================================================================
     async function testtree() {
         const testDB = new TestDB();
         testDB.dbJsonLoad(testPDBJson);
-        reco.ui.layout.RootPanel.addRootFullPanel("tree");
-        new TreeUI(document.getElementById("tree") as HTMLElement, new GeoTreeHandler(testDB)).display();
+        reco.ui.layout.RootPanel.addRootFullPanel("root");
+        let layout = new LayoutNcSc("root")
+        layout.display();
+        new TreeUI(layout.northCenter, new GeoTreeHandler(testDB)).display();
     }
     // ============================================================================================
     testtree()
