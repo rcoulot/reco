842a260985b8a3c6d1e9f5d0a25f3049946d033f
rcoulot <rcoulot@gmail.com>
2025-11-11 23:57:35 +0100

    SVGList ok

--- _client/dist/typescript/test/test-svg.ts
+++ _client/dist/typescript/test/test-svg.ts
@@ -42,6 +42,10 @@ namespace reco.ui.svg {
             });
         }
         // ----------------------------------------------------------------------------------------
+        refresh() {
+            for (let group of this.childrenGroups) group.refresh();
+        }
+        // ----------------------------------------------------------------------------------------
         border(size: number, color: string): SvgRoot {
             this.elt.setAttribute("style", "border:" + size + "px solid " + color);
             return this;
@@ -80,12 +84,15 @@ namespace reco.ui.svg {
         // ----------------------------------------------------------------------------------------
         id: string;
         svgRoot: SvgRoot;
+        eltBorder: SVGRectElement;
         elt: SVGGElement;
         childrenItems: SvgItem[] = [];
         childrenItemsById: { [id: string]: SvgItem } = {};
         childrenElements: SVGElement[] = [];
         x: number;
         y: number;
+        borderSize: number = 1;
+        borderColor: string = "transparent";
         // ----------------------------------------------------------------------------------------
         constructor(svgRoot: SvgRoot, x: number, y: number) {
             this.svgRoot = svgRoot;
@@ -98,7 +105,24 @@ namespace reco.ui.svg {
             this.svgRoot.childrenGroups.push(this);
             this.svgRoot.childrenElements.push(this.elt);
             this.svgRoot.childrenGroupById[this.id] = this;
+            this.eltBorder = document.createElementNS("http://www.w3.org/2000/svg", "rect") as any;
+            this.eltBorder.setAttribute("x", "" + 0);
+            this.eltBorder.setAttribute("y", "" + 0);
+            this.eltBorder.setAttribute("width", "" + 0);
+            this.eltBorder.setAttribute("height", "" + 0);
+            this.eltBorder.setAttribute('fill', 'transparent');
+            this.elt.appendChild(this.eltBorder);
         }
+        // ----------------------------------------------------------------------------------------
+        refresh() {
+            for (let item of this.childrenItems) item.refresh();
+            this.border(this.borderColor, this.borderSize)
+        }
+        // ----------------------------------------------------------------------------------------
+        get eltX(): number { return this.elt.getBoundingClientRect().x - this.svgRoot.elt.getBoundingClientRect().x; }
+        get eltY(): number { return this.elt.getBoundingClientRect().y - this.svgRoot.elt.getBoundingClientRect().y; }
+        get eltHeight(): number { return this.elt.getBoundingClientRect().height; }
+        get eltWidth(): number { return this.elt.getBoundingClientRect().width; }
         // ----------------------------------------------------------------------------------------
         moveTo(x: number, y: number): SvgGroup {
             this.x = x;
@@ -113,13 +137,20 @@ namespace reco.ui.svg {
         }
         // ----------------------------------------------------------------------------------------
         fill(color: string): SvgGroup {
-            this.elt.style.fill = color;
+            this.eltBorder.style.fill = color;
             return this;
         }
         // ----------------------------------------------------------------------------------------
-        border(size: number, color: string): SvgGroup {
-            this.elt.style.strokeWidth = "" + size;
-            this.elt.style.stroke = color;
+        border(color: string = "black", size: number = 1): SvgGroup {
+            this.borderColor = color;
+            this.borderSize = size;
+            const bbox = this.elt.getBBox();
+            this.eltBorder.setAttribute('x', (bbox.x - 2).toString());
+            this.eltBorder.setAttribute('y', (bbox.y - 2).toString());
+            this.eltBorder.setAttribute('width', (bbox.width + 2 * 2).toString());
+            this.eltBorder.setAttribute('height', (bbox.height + 2 * 2).toString());
+            this.eltBorder.setAttribute('stroke', color);
+            this.eltBorder.setAttribute('stroke-width', size.toString());
             return this;
         }
         // ----------------------------------------------------------------------------------------
@@ -133,7 +164,7 @@ namespace reco.ui.svg {
         // ----------------------------------------------------------------------------------------
         constructor(svgGroup: SvgGroup, type: string, x: number, y: number) {
             this.svgGroup = svgGroup;
-            this.id = this.svgGroup.id + "-" + this.svgGroup.svgRoot.SEQ++;
+            this.id = this.svgGroup.svgRoot.id + "-" + this.svgGroup.svgRoot.SEQ++;
             this.elt = document.createElementNS("http://www.w3.org/2000/svg", type) as any;
             this.elt.setAttribute("id", this.id);
             this.svgGroup.childrenItems.push(this);
@@ -141,19 +172,15 @@ namespace reco.ui.svg {
             this.svgGroup.childrenItemsById[this.elt.id] = this;
         }
         // ----------------------------------------------------------------------------------------
+        get eltX(): number { return this.elt.getBoundingClientRect().x - this.svgGroup.elt.getBoundingClientRect().x; }
+        get eltY(): number { return this.elt.getBoundingClientRect().y - this.svgGroup.elt.getBoundingClientRect().y; }
+        get eltHeight(): number { return this.elt.getBoundingClientRect().height; }
+        get eltWidth(): number { return this.elt.getBoundingClientRect().width; }
+        // ----------------------------------------------------------------------------------------
+        refresh() { }
+        // ----------------------------------------------------------------------------------------
         add(): X {
             this.svgGroup.elt.appendChild(this.elt);
-            return this as unknown as X;
-        }
-        // ----------------------------------------------------------------------------------------
-        fill(color: string): X {
-            this.elt.style.fill = color;
-            return this as unknown as X;
-        }
-        // ----------------------------------------------------------------------------------------
-        border(size: number, color: string): X {
-            this.elt.style.strokeWidth = "" + size;
-            this.elt.style.stroke = color;
             return this as unknown as X;
         }
         // ----------------------------------------------------------------------------------------
@@ -172,11 +199,23 @@ namespace reco.ui.svg {
             this.elt.setAttribute("y", "" + y);
             this.elt.setAttribute("width", "" + w);
             this.elt.setAttribute("height", "" + h);
+            this.elt.setAttribute('fill', 'transparent');
         }
         // ----------------------------------------------------------------------------------------
         roundCorner(r: number): RectItem {
             this.elt.setAttribute("rx", "" + r);
             this.elt.setAttribute("ry", "" + r);
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        fill(color: string): RectItem {
+            this.elt.style.fill = color;
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        border(size: number, color: string): RectItem {
+            this.elt.style.strokeWidth = "" + size;
+            this.elt.style.stroke = color;
             return this;
         }
         // ----------------------------------------------------------------------------------------
@@ -194,14 +233,169 @@ namespace reco.ui.svg {
             this.elt.setAttribute("r", "" + r);
         }
         // ----------------------------------------------------------------------------------------
+        fill(color: string): CircleItem {
+            this.elt.style.fill = color;
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        border(size: number, color: string): CircleItem {
+            this.elt.style.strokeWidth = "" + size;
+            this.elt.style.stroke = color;
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+    }
+    // ============================================================================================
+    export class TextItem extends SvgItem<TextItem, SVGTextElement> {
+        // ----------------------------------------------------------------------------------------
+        constructor(svgGroup: SvgGroup, x: number = 0, y: number = 0, text: string) {
+            super(svgGroup, "text", x, y);
+            this.elt.textContent = text;
+            if (x == 0 && y == 0) {
+                let lastItem = this.svgGroup.childrenItems[this.svgGroup.childrenItems.length - 2];
+                y = lastItem ? lastItem.eltY + lastItem.eltHeight + 2 : 0;
+            }
+            console.log("TextItem y=" + y + " > " + text);
+            this.elt.setAttribute("x", "" + x);
+            this.elt.setAttribute("y", "" + y);
+            this.elt.setAttribute('font-family', 'Times New Roman');
+            this.elt.setAttribute('font-size', "12");
+            this.elt.setAttribute('font-weight', "normal");
+            this.elt.setAttribute('letter-spacing', "1px");
+        }
+        // ----------------------------------------------------------------------------------------
+        font(weight: string = "normal", size: number = 12): TextItem {
+            this.elt.setAttribute('font-size', "" + size);
+            this.elt.setAttribute('font-weight', weight);
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        color(color: string): TextItem {
+            this.elt.style.stroke = color;
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+    }
+    // ============================================================================================
+    export class SeparatorItem extends SvgItem<SeparatorItem, SVGLineElement> {
+        // ----------------------------------------------------------------------------------------
+        constructor(svgGroup: SvgGroup, afterItemIndex: number = -1) {
+            super(svgGroup, "line", 0, 0);
+            if (afterItemIndex === -1) afterItemIndex = this.svgGroup.childrenItems.length - 2;
+            let afterItem = this.svgGroup.childrenItems[afterItemIndex];
+            let y = afterItem ? afterItem.eltY + 5 : 0;
+            console.log("SeparatorItem y=" + y);
+            this.elt.setAttribute("x1", "" + 0);
+            this.elt.setAttribute("y1", "" + y);
+            this.elt.setAttribute("x2", "" + this.svgGroup.eltWidth);
+            this.elt.setAttribute("y2", "" + y);
+        }
+        // ----------------------------------------------------------------------------------------
+        color(color: string): SeparatorItem {
+            this.elt.style.stroke = color;
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        refresh() {
+            this.elt.setAttribute("x2", "" + this.svgGroup.eltWidth);
+        }
+        // ----------------------------------------------------------------------------------------
+    }
+    // ============================================================================================
+    export class SvgList extends SvgGroup {
+        // ----------------------------------------------------------------------------------------
+        strokeColor: string = "black";
+        titleColor: string = "black";
+        lineColor: string = "blue";
+        // titleFillColor: string = "#f0f0f0";
+        titleFillColor: string = "lightyellow";
+        eltTitleBorder: SVGRectElement;
+        borderTitleSize: number = 1;
+        borderTitleColor: string = "transparent";
+        titleItem: TextItem | null = null;
+        // ----------------------------------------------------------------------------------------
+        constructor(svgRoot: SvgRoot, x: number, y: number) {
+            super(svgRoot, x, y);
+            this.fill("transparent").border(this.strokeColor).add();
+            this.eltTitleBorder = document.createElementNS("http://www.w3.org/2000/svg", "rect") as any;
+            this.eltTitleBorder.setAttribute("x", "" + 0);
+            this.eltTitleBorder.setAttribute("y", "" + 0);
+            this.eltTitleBorder.setAttribute("width", "" + 0);
+            this.eltTitleBorder.setAttribute("height", "" + 0);
+            this.eltTitleBorder.setAttribute('fill', 'transparent');
+            this.elt.appendChild(this.eltTitleBorder);
+            this.fillTitle(this.titleFillColor);
+        }
+        // ----------------------------------------------------------------------------------------
+        refresh(): void {
+            super.refresh();
+            this.borderTitle(this.borderTitleColor, this.borderTitleSize);
+        }
+        // ----------------------------------------------------------------------------------------
+        title(text: string, color: string = this.titleColor): SvgList {
+            this.titleColor = color;
+            this.titleItem = new TextItem(this, 0, 0, text).font("bolder", 16).color(color).add();
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        fillTitle(color: string): SvgList {
+            this.eltTitleBorder.style.fill = color;
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        borderTitle(color: string = "black", size: number = 1): SvgList {
+            this.borderTitleColor = color;
+            this.borderTitleSize = size;
+            const bbox = this.elt.getBBox();
+            this.eltTitleBorder.setAttribute('x', (bbox.x).toString());
+            this.eltTitleBorder.setAttribute('y', (bbox.y).toString());
+            this.eltTitleBorder.setAttribute('width', (bbox.width).toString());
+            this.eltTitleBorder.setAttribute('height', (this.titleItem!.elt.getBoundingClientRect().height + 2*2).toString());
+            this.eltTitleBorder.setAttribute('stroke', color);
+            this.eltTitleBorder.setAttribute('stroke-width', size.toString());
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        separator(): SvgList {
+            new SeparatorItem(this).color(this.strokeColor).add();
+            return this;
+        }
+        // ----------------------------------------------------------------------------------------
+        line(text: string, color: string = this.lineColor): SvgList {
+            this.lineColor = color;
+            new TextItem(this, 0, 0, text).color(color).add();
+            return this;
+        };
     }
     // ============================================================================================
     const svg: SvgRoot = new SvgRoot("mySvg", "70vw", "70vh").border(1, "blue");
-    const group1: SvgGroup = new SvgGroup(svg, 10, 10).fill("lightgray").add();
-    new RectItem(group1, 0, 0, 40, 20).fill("blue").border(1, "red").add();
-    const group2: SvgGroup = new SvgGroup(svg, 100, 100).fill("lightgray").add();
-    const r2 = new RectItem(group2, 0, 0, 80, 40).fill("green").border(1, "red").add();
-    new CircleItem(group2, 30, 20, 10).fill("green").border(1, "yellow").add();
+
+    // const group1: SvgGroup = new SvgGroup(svg, 10, 10).add();
+    // new RectItem(group1, 0, 0, 40, 20).fill("blue").border(1, "red").add();
+    // const group2: SvgGroup = new SvgGroup(svg, 100, 100).add();
+    // const r2 = new RectItem(group2, 0, 0, 80, 40).fill("green").border(1, "red").add();
+    // new CircleItem(group2, 30, 20, 10).fill("green").border(1, "yellow").add();
+
+    new SvgList(svg, 50, 50)
+        .title("(c) Person")
+        .separator()
+        .line("(a) firstname")
+        .line("(a) lastname")
+        .line("(a) birthdate")
+        .add();
+
+    new SvgList(svg, 200, 50)
+        .title("(c) Address")
+        .separator()
+        .line("(a) lines")
+        .line("(a) city")
+        .line("(a) zip")
+        .line("(a) state")
+        .line("(a) country")
+        .add();
+
+
+    svg.refresh();
     // ============================================================================================
 }
 // ################################################################################################
